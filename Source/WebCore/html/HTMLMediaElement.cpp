c7ab0b8a7764761a10e22cd7726d4dcc217dfabd
Jer Noble <jer.noble@apple.com>
2022-09-26 18:07:40 -0700

    [iOS] Remote playback of autoplaying & muted videos stops when device is locked https://bugs.webkit.org/show_bug.cgi?id=245238 <rdar://98685762>
    Reviewed by Eric Carlson.
    
    Modify `canProduceAudio()` to take volume state into account, and only take mute state into
    account for non-GStreamer ports.
    
    Then, use `canProduceAudio()` to calculate the IsPlayingAudio state to report upward to the
    Document's mediaState.
    
    This results in the Document (and UIProcess) reporting that the page is performing audible
    playback, and keeps the WebContent/GPU processes from suspending when in app background state.
    
    * Source/WebCore/html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::mediaType const):
    (WebCore::HTMLMediaElement::canProduceAudio const):
    (WebCore::HTMLMediaElement::mediaState const):
    
    Canonical link: https://commits.webkit.org/254887@main

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -7927,7 +7927,7 @@ PlatformMediaSession::MediaType HTMLMediaElement::mediaType() const
 {
     if (m_player && m_readyState >= HAVE_METADATA) {
         auto hasVideo = this->hasVideo();
-        if (hasVideo && hasAudio() && !muted())
+        if (hasVideo && canProduceAudio())
             return PlatformMediaSession::MediaType::VideoAudio;
         return hasVideo ? PlatformMediaSession::MediaType::Video : PlatformMediaSession::MediaType::Audio;
     }
@@ -7968,8 +7968,16 @@ bool HTMLMediaElement::canProduceAudio() const
     if (isSuspended())
         return false;
 
+    if (!volume())
+        return false;
+
+    // For GStreamer ports the semantics of IsPlayingAudio slightly differ from Apple ports. The
+    // webkit_web_view_is_playing_audio() API is expected to return true if a page is producing
+    // audio even though it might be muted.
+#if !USE(GSTREAMER)
     if (muted())
         return false;
+#endif
 
     if (m_player && m_readyState >= HAVE_METADATA)
         return hasAudio();
@@ -8229,14 +8237,7 @@ MediaProducerMediaStateFlags HTMLMediaElement::mediaState() const
     if (!isPlaying())
         return state;
 
-    // For GStreamer ports the semantics of IsPlayingAudio slightly differ from Apple ports. The
-    // webkit_web_view_is_playing_audio() API is expected to return true if a page is producing
-    // audio even though it might be muted.
-    bool isPlayingAudio = hasAudio && volume();
-#if !USE(GSTREAMER)
-    isPlayingAudio = isPlayingAudio && !muted();
-#endif
-    if (isPlayingAudio)
+    if (canProduceAudio())
         state.add(MediaProducerMediaState::IsPlayingAudio);
 
     if (hasActiveVideo)
