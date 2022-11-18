105d16c98b731b3c4deb4c79f2f262c825bd6731
Jer Noble <jer.noble@apple.com>
2022-11-18 11:52:28 -0800

    REGRESSION (246057@main): [ macOS wk1 ] imported/w3c/web-platform-tests/video-rvfc/request-video-frame-callback.html is a flaky failure https://bugs.webkit.org/show_bug.cgi?id=244736 rdar://99507721
    Reviewed by Eric Carlson.
    
    HTMLMediaElement's updateShouldAutoplay() will check, among other things, whether the media element
    is currently visible in the viewport. However, when this method is called from within will- and
    didAttachRenderers, the renderer either has not yet been created, or layout has not yet occurred.
    Similarly, calling this method from within styleDidChange/visibilityDidChange poses a similar
    problem, where the element may have been made visible but layout has not yet run.
    
    Add a new method, scheduleUpdateShouldAutoplay(), which will re-check autoplay status on the next
    run loop, and use this method instead of updateShouldAutoplay() everywhere.
    
    * LayoutTests/platform/mac-wk1/TestExpectations:
    * Source/WebCore/html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::didAttachRenderers):
    (WebCore::HTMLMediaElement::didDetachRenderers):
    (WebCore::HTMLMediaElement::visibilityDidChange):
    (WebCore::HTMLMediaElement::cancelPendingTasks):
    (WebCore::HTMLMediaElement::isVisibleInViewportChanged):
    (WebCore::HTMLMediaElement::scheduleUpdateShouldAutoplay):
    * Source/WebCore/html/HTMLMediaElement.h:
    
    Canonical link: https://commits.webkit.org/256846@main

--- Source/WebCore/html/HTMLMediaElement.cpp
+++ Source/WebCore/html/HTMLMediaElement.cpp
@@ -935,7 +935,7 @@ void HTMLMediaElement::didAttachRenderers()
         if (m_mediaSession && m_mediaSession->wantsToObserveViewportVisibilityForAutoplay())
             renderer->registerForVisibleInViewportCallback();
     }
-    updateShouldAutoplay();
+    scheduleUpdateShouldAutoplay();
 }
 
 void HTMLMediaElement::willDetachRenderers()
@@ -946,7 +946,7 @@ void HTMLMediaElement::willDetachRenderers()
 
 void HTMLMediaElement::didDetachRenderers()
 {
-    updateShouldAutoplay();
+    scheduleUpdateShouldAutoplay();
 }
 
 void HTMLMediaElement::didRecalcStyle(Style::Change)
@@ -4746,7 +4746,7 @@ void HTMLMediaElement::layoutSizeChanged()
 
 void HTMLMediaElement::visibilityDidChange()
 {
-    updateShouldAutoplay();
+    scheduleUpdateShouldAutoplay();
 }
 
 void HTMLMediaElement::setSelectedTextTrack(TextTrack* trackToSelect)
@@ -5830,6 +5830,7 @@ void HTMLMediaElement::cancelPendingTasks()
     m_resumeTaskCancellationGroup.cancel();
     m_seekTaskCancellationGroup.cancel();
     m_playbackControlsManagerBehaviorRestrictionsTaskCancellationGroup.cancel();
+    m_updateShouldAutoplayTaskCancellationGroup.cancel();
 #if !HAVE(MEDIA_VOLUME_PER_ELEMENT)
     m_volumeRevertTaskCancellationGroup.cancel();
 #endif
@@ -8411,6 +8412,16 @@ void HTMLMediaElement::isVisibleInViewportChanged()
         mediaSession().isVisibleInViewportChanged();
         updateShouldAutoplay();
         schedulePlaybackControlsManagerUpdate();
+    });
+}
+
+void HTMLMediaElement::scheduleUpdateShouldAutoplay()
+{
+    if (m_updateShouldAutoplayTaskCancellationGroup.hasPendingTask())
+        return;
+
+    queueCancellableTaskKeepingObjectAlive(*this, TaskSource::MediaElement, m_updateShouldAutoplayTaskCancellationGroup, [this] () {
+        updateShouldAutoplay();
     });
 }
 
