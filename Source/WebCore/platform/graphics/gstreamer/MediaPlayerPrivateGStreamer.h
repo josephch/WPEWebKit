338cd5fafbd0cc759cfd72d1c0e9b6295b42f53c
Philippe Normand <philn@igalia.com>
2023-01-09 04:32:54 -0800

    [GStreamer][WebRTC] Improved statistics support https://bugs.webkit.org/show_bug.cgi?id=249940
    Reviewed by Xabier Rodriguez-Calvar.
    
    Fill the `kind` attribute in RtpStreamStats and also a few more video frame related attributes for
    inbound rtp stats. In order to support extended stats gathering, the queryDecodedVideoFramesCount()
    virtual method in VideoFrameObserver was replaced by a more generic queryAdditionalStats method.
    
    * Source/WebCore/Modules/mediastream/gstreamer/GStreamerStatsCollector.cpp:
    (WebCore::fillRTCRTPStreamStats):
    (WebCore::fillInboundRTPStreamStats):
    (WebCore::fillRTCTransportStats):
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::configureVideoDecoder):
    (WebCore::MediaPlayerPrivateGStreamer::updateVideoSinkStatistics):
    (WebCore::MediaPlayerPrivateGStreamer::videoPlaybackQualityMetrics):
    * Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    * Source/WebCore/platform/mediastream/RealtimeMediaSource.h:
    * Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp:
    * Source/WebCore/platform/mediastream/gstreamer/RealtimeIncomingVideoSourceGStreamer.cpp:
    (WebCore::RealtimeIncomingVideoSourceGStreamer::stats):
    
    Canonical link: https://commits.webkit.org/258659@main

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -435,6 +435,8 @@ protected:
     void incrementDecodedVideoFramesCount() { m_decodedVideoFrames++; }
     uint64_t decodedVideoFramesCount() const { return m_decodedVideoFrames; }
 
+    bool updateVideoSinkStatistics();
+
 private:
     class TaskAtMediaTimeScheduler {
     public:
